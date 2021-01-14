858ec2d593e3d348a4f985287bb43798c368cea4
Suphawit Auchariyamet <s6201012620333@email.kmutnb.ac.th>
2021-01-14 19:29:07 +0700

    table create

--- cgi-bin/calcu.py
+++ cgi-bin/calcu.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3 
 print("Content-Type: text/html")    
 print()                             
- 
+
 import cgi,cgitb
 import time
 cgitb.enable() #for debugging
@@ -9,10 +9,18 @@ cgitb.enable() #for debugging
 form = cgi.FieldStorage()
 temp_input = form.getvalue('temp')
 ttime = time.ctime()
-
 f = open("temp.txt", "a")
-f.write("<br>" + temp_input)
-f.write("<br>" + ttime)
+f.write("<style>")
+f.write("table, th, td {")
+f.write("border: 1px solid black;")
+f.write("border-collapse: collapse;}")
+f.write("</style>")
+f.write("<table>")
+f.write("<tr>")
+f.write("<td>" +temp_input +"</td>")
+f.write("<td>" +ttime+"</td>")
+f.write("</tr>")
+f.write("</table>")
 f.close()
 
 #open and read the file after the appending:
