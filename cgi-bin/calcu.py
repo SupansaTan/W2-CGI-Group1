232e68444333ce69b6b86e4f1e027e61ecdb0418
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-01-14 16:18:01 +0700

    Add the current time

--- cgi-bin/calcu.py
+++ cgi-bin/calcu.py
@@ -3,13 +3,16 @@ print("Content-Type: text/html")
 print()                             
  
 import cgi,cgitb
+import time
 cgitb.enable() #for debugging
 
 form = cgi.FieldStorage()
 temp_input = form.getvalue('temp')
+ttime = time.ctime()
 
 f = open("temp.txt", "a")
 f.write("<br>" + temp_input)
+f.write("<br>" + ttime)
 f.close()
 
 #open and read the file after the appending:
