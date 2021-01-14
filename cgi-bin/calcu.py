a81878dd1aab62c3a36933ae5e7c7c3ea89b82b4
Suphawit Auchariyamet <s6201012620333@email.kmutnb.ac.th>
2021-01-14 14:29:30 +0700

    calculator can calculate all

--- cgi-bin/calcu.py
+++ cgi-bin/calcu.py
@@ -8,7 +8,16 @@ cgitb.enable() #for debugging
 form = cgi.FieldStorage()
 num1= form.getvalue('fnum')
 num2= form.getvalue('snum')
-num3=int(num1)+int(num2)
+opr=form.getvalue('oprnum')
+if opr=="+":
+    num3=int(num1)+int(num2)
+elif opr=="-":
+    num3=int(num1)-int(num2)
+elif opr=="*":
+    num3=int(num1)*int(num2)
+elif opr=="/":
+    num3=int(num1)/int(num2)
+
 print("<h1>This is First Number %s</h1>" % (num1))
 print("<h1>This is Second Number %s</h1>" % (num2))
 print("<h1>Answer is %s</h1>"%(num3)) 
