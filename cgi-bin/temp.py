20ca066dc1c1fc3db0e55936e227074cc2eec643
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-01-17 00:08:47 +0700

    Can show form with table in one page and append a row in the table after submitted form

--- cgi-bin/temp.py
+++ cgi-bin/temp.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3 
-print("Content-Type: text/html")
-print("<html>")
-print()
 
 import cgi,cgitb
 from datetime import *
 cgitb.enable()
 body = ""
-                        
-# get value : form
-form = cgi.FieldStorage()
-temp_input = form.getvalue('temp')
 
-# date & time
-now = datetime.now()
-date_now = now.strftime("%d/%m/%Y")
-time_now = now.strftime("%H:%M:%S")
-
-
-# write file
-f = open("temp.txt", "a+")
-f.write(date_now + "," + time_now + "," + str(temp_input) + "\n")
-f.close()
-
-# open and read the file after the appending:
-f = open("temp.txt", "r")
-read_file = f.readlines()
- 
-body += """
-        <link rel="stylesheet" href="/temp.css">
+def generate_form():
+    form_body = \
+        """
+        <link rel="stylesheet" href="/css/temp.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
         <title>Temperature</title>
         <body>
+        <div class="form-container">
+            <form name="pyform" method="POST" action="/cgi-bin/temp.py">
+                <input type="number" name="temp"/><br><br>
+                <input type="submit" id="submit_btn" name="submit" value="Submit"/><br>
+            </form>
+        </div>
+        """
+    return form_body
+
+def write_file(temp):
+    now = datetime.now()
+    date_now = now.strftime("%d/%m/%Y")
+    time_now = now.strftime("%H:%M:%S")
+
+    f = open("temp.txt", "a+")
+    f.write(date_now + "," + time_now + "," + str(temp) + "\n")
+    f.close()
+
+def read_file_before_submit():
+    f = open("temp.txt", "r")
+    read = f.readlines()
+    
+    # part of head table (topics)
+    table_body = \
+        """
         <div class="table-container">
             <table id="temp-table">
                 <tr>
@@ -42,17 +47,55 @@ body += """
                 </tr>
         """
 
-for i in range(len(read_file)):
-    split_values = read_file[i].split(",")
+    # part of body table 
+    for i in range(len(read)):
+        split_values = read[i].split(",")
+        date = split_values[0]
+        time = split_values[1]
+        temp = split_values[2]
+
+        table_body += \
+            """ <tr>
+                    <td class="column1">{0}</td>
+                    <td class="column2">{1}</td>
+                    <td class="column3">{2}</td>
+                </tr>
+            """.format(date, time, temp)
+    
+    return table_body
+
+def read_file_after_submitted():
+    f = open("temp.txt", "r")
+    read = f.readlines()
+
+    # read only line that before the last line
+    split_values = read[len(read)-1].split(",")
     date = split_values[0]
     time = split_values[1]
     temp = split_values[2]
 
-    body += """ <tr><td class="column1">{0}</td>
+    table_body = \
+        """ <tr>
+                <td class="column1">{0}</td>
                 <td class="column2">{1}</td>
                 <td class="column3">{2}</td>
-                </tr>
-            """.format(date, time, temp)
-     
-print(body) 
-print("</table></div></body></html>")
+            </tr>
+        """.format(date, time, temp)
+    return table_body
+
+# run the first time
+if __name__ == "__main__":
+    print("Content-Type: text/html")
+    print("<html>")
+    print()
+    body += generate_form()
+    body += read_file_before_submit()
+    print(body)
+
+# after submitted the form then append temperature in file txt 
+# and add row in the table
+form = cgi.FieldStorage()
+temp_input = form.getvalue('temp')
+if temp_input != None:
+    write_file(temp_input)
+    print(read_file_after_submitted())
