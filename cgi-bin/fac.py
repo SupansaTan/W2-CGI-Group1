084aede1d0820dbc10d0fa77f73b2edd2cadd689
Suphawit Auchariyamet <s6201012620333@email.kmutnb.ac.th>
2021-01-21 16:47:16 +0700

    fac fix

--- cgi-bin/fac.py
+++ cgi-bin/fac.py
@@ -8,20 +8,21 @@ print("""
   <title>Test</title>
 </head>
 <body>
-  <form name="pyform" method="POST" action="/cgi-bin/plus.py">
+  <form name="pyform" method="POST" action="/cgi-bin/fac.py">
   Num1:&emsp;<input type="number" name="fnum"/><br/><br/>
     <input type="submit" name="submit" value="Submit"/><br/>
   </form>
 </body>
 """)
 import cgi,cgitb
+
 cgitb.enable() #for debugging
 
 form = cgi.FieldStorage()
-num= form.getvalue('fnum')
+num=form.getvalue('fnum')
 fac = 1
- 
-for i in range(1, int(num) + 1):
-	fac = fac * i
-print("<h1>Answer is %s</h1>"%(fac)) 
+if num != None: 
+  for i in range(1,(int(num)+1)):
+	  fac = fac * i
+  print("<h1>Answer is %s</h1>"%(fac)) 
 print("</html>")
